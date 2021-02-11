faea39790d99bd74fda8fb633361f64b7fff54e9
Ziyad E. El Baz <ziyad.elbaz@me.com>
2021-02-11 00:12:03 -0600

    m

--- background.js
+++ background.js
@@ -1,25 +1,38 @@
+/* eslint-disable no-undef */
 // sense if the tab is active
 
-chrome.tabs.query({
-    active:true,
-    currentWindow: true
-}, function(tabs) {
-    var tab = tabs[0];
-    var url = tabs.url;
-  console.log(url)
-})
+chrome.tabs.onActivated.addListener((tab) => {
+  // start a new session if target url
+  chrome.tabs.get(tab.tabId, (current_tab_info) => {
+    if (current_tab_info.url.includes('leetcode.com')) {
+      console.log(current_tab_info.url);
+    }
+  });
+});
+
+// Evaluate if this is a first visit
+let data = localStorage.getItem('data');
+if (!data) {
+  localStorage.setItem('data', data);
+  localStorage.setItem('total', 0);
+}
+
+// load existing data
+data = localStorage.getItem('data');
+let total = localStorage.getItem('total') ? ;
 
 
-// chrome.tabs.onActivated.addListener(tab => {
-//   //start a new session if target url
-//   chrome.tabs.get(tab.tabId, current_tab_info => {
-    
-//     if(current_tab_info.url.includes('leetcode.com')){
-//        console.log(current_tab_info.url)
-//     }
-//   })
-    
-// })
 
-// get the tab to make sure it's one of our domains 
-// console.log domain
+
+
+
+
+
+
+
+
+// function for saving our data
+function saveData (totes, obj) { 
+  localStorage.setItem('data', obj);
+  localStorage.setItem('total', totes);
+}
